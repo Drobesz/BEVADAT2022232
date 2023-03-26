e681cdb672e56b868909d82148a6cb8ed5fd3671
Márton Drobina <drobesz@windowslive.com>
2023-03-27 01:45:59 +0200

    Done every task in HW4, except the chart craps

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 def csv_to_df(file): return pd.read_csv(file)
 def capitalize_columns(df):
@@ -23,6 +24,18 @@ def add_grade(df):
                         np.where((new_df['math score'] + new_df['reading score'] + new_df['writing score']) < 240, 'C',
                         np.where((new_df['math score'] + new_df['reading score'] + new_df['writing score']) < 270, 'B', 'A'))))
     return new_df
+def add_age(df):
+    new_df = df.copy()
+    random.seed(42)
+    ages = [random.randint(18, 67) for _ in range(len(new_df))]
+    return pd.concat([new_df, pd.DataFrame({'age': ages})], axis=1)
+def female_top_score(df):
+    new_df = df.copy()
+    new_df = new_df[new_df['gender'] == 'female']
+    new_df['allscore'] = new_df['math score'] + new_df['reading score'] + new_df['writing score']
+    return (int(new_df.loc[[new_df['allscore'].idxmax()]]['math score']), 
+            int(new_df.loc[[new_df['allscore'].idxmax()]]['reading score']), 
+            int(new_df.loc[[new_df['allscore'].idxmax()]]['writing score']))
 
 '''
 testdata = csv_to_df('HAZI\HAZI04\StudentsPerformance.csv')
@@ -31,4 +44,6 @@ print(math_passed_count(testdata))
 print(did_pre_course(testdata))
 print(average_scores(testdata))
 print(add_grade(testdata))
+print(female_top_score(testdata))
+print(add_age(testdata))
 '''
