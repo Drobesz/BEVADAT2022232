4e339911c1d989cfdc166bcfad1fafcfdb326fd0
Márton Drobina <drobesz@windowslive.com>
2023-04-02 19:10:21 +0200

    Test the HW and lecture 5

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -1,10 +1,9 @@
-import numpy as np
 import seaborn as sns
+import numpy as np
+import pandas as pd
 from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
-
-np.random.seed(42)
 
 class KNNClassifier:
     
@@ -16,7 +15,7 @@ class KNNClassifier:
         self.k = k
         self.test_split_ratio = test_split_ratio
     
-    def train_test_split(self, features : np.ndarray, labels : np.ndarray) -> None:
+    def train_test_split(self, features : pd.DataFrame, labels : pd.DataFrame) -> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert len(features) == test_size + train_size, "Size mismatch!"
@@ -24,10 +23,10 @@ class KNNClassifier:
         self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
         self.x_test, self.y_test = features[train_size : train_size + test_size, :], labels[train_size : train_size + test_size]
     
-    def euclidean(self, element_of_x : np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis = 1))
+    def euclidean(self, element_of_x : pd.DataFrame) -> pd.DataFrame:
+        return ((self.x_train - element_of_x)**2).sum(axis=1).pow(1./2)
     
-    def predict(self, x_test:np.ndarray) -> None:
+    def predict(self, x_test:pd.DataFrame) -> None:
         labels_pred = []
         for x_test_element in x_test:
             distances = self.euclidean(x_test_element)
@@ -44,9 +43,8 @@ class KNNClassifier:
         return confusion_matrix(self.y_test, self.y_preds)
 
     @classmethod
-    def load_csv(csv_path : str) -> Tuple[np.ndarray, np.ndarray]:
-        np.random.seed(42)
-        dataset = np.genfromtxt(csv_path, delimiter = ',')
-        np.random.shuffle(dataset,)
+    def load_csv(csv_path : str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        dataset = pd.read_csv(csv_path, delimiter = ',', engine='python')
+        dataset = dataset.sample(frac=1, random_state = 42)
         x, y = dataset[:, :4],dataset[:, -1]
         return x, y
